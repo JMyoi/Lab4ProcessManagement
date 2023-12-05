9f15a340bf3ef35953e246b40fe22ecd422e485a
Jay Chen <jaychenjjc@gmail.com>
2023-12-05 01:06:32 -0500

    last one

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -41,11 +41,113 @@ void FCFS(vector<Process> readyQueue) {
 }
 
 void Priority(vector<Process> readyQueue) {
+	int totalTurnaround(0);
+	float avgTurnaround;
+	int timeRemaining[100];
+	int isCompleted[100];
+	memset(isCompleted, 0, sizeof(isCompleted));
+	for (int i = 0; i < readyQueue.size(); i++) {
+		timeRemaining[i] = readyQueue.at(i).burstTime;
+	}
+	int currentTime(0), completed(0);
 
+	while (completed != readyQueue.size()) {
+		int index(-1), max(100);
+		for (int i = 0; i < readyQueue.size(); i++) {
+			if (readyQueue.at(i).arrivalTime <= currentTime && isCompleted[i] == 0) {
+				if (readyQueue.at(i).priority < max) {
+					max = readyQueue.at(i).priority;
+					index = i;
+				}
+				if (readyQueue.at(i).priority == max) {
+					if (readyQueue.at(i).arrivalTime < readyQueue.at(index).arrivalTime) {
+						max = readyQueue.at(i).priority;
+						index = i;
+					}
+				}
+			}
+		}
+		if (index != -1) {
+			if (timeRemaining[index] == readyQueue.at(index).burstTime) {
+				readyQueue.at(index).startTime = currentTime;
+			}
+			timeRemaining[index] -= 1;
+			currentTime++;
+			if (timeRemaining[index] == 0) {
+				readyQueue.at(index).completionTime = currentTime;
+				readyQueue.at(index).turnaroundTime = readyQueue.at(index).completionTime - readyQueue.at(index).arrivalTime;
+				totalTurnaround += readyQueue.at(index).turnaroundTime;
+				isCompleted[index] = 1;
+				completed++;
+			}
+		}
+		else {
+			currentTime++;
+		}
+	}
+	avgTurnaround = (float)totalTurnaround / readyQueue.size();
+	cout << "\nPID\tArrival Time\tStart Time\t Completion Time\tTurnaround Time\t\n";
+	for (int i = 0; i < readyQueue.size(); i++) {
+		cout << readyQueue.at(i).id << "\t" << readyQueue.at(i).arrivalTime << "\t\t" << readyQueue.at(i).startTime << "\t\t" << readyQueue.at(i).completionTime << "\t\t" << readyQueue.at(i).turnaroundTime << endl;
+	}
+
+	cout << "Average Turnaround Time = " << avgTurnaround << endl;
+
+	
 }
+
+
 void SJN(vector<Process> readyQueue) {
+	int isCompleted[100];
+	int totalTurnaround(0);
+	memset(isCompleted, 0, sizeof(isCompleted));
+	int currentTime(0), completed(0);
+
+
+	while (completed != readyQueue.size()) {
+		int index(-1), max(100);
+		for (int i = 0; i < readyQueue.size(); i++) {
+			if (readyQueue.at(i).arrivalTime <= currentTime && isCompleted[i] == 0) {
+				if (readyQueue.at(i).burstTime < max) {
+					max = readyQueue.at(i).burstTime;
+					index = i;
+				}
+				if (readyQueue.at(i).burstTime == max) {
+					if (readyQueue.at(i).arrivalTime < readyQueue.at(index).arrivalTime) {
+						max = readyQueue.at(i).burstTime;
+						index = i;
+					}
+				}
+			}
+		}
+
+		if (index != -1) {
+			readyQueue.at(index).startTime = currentTime;
+			readyQueue.at(index).completionTime = readyQueue.at(index).startTime + readyQueue.at(index).burstTime;
+			readyQueue.at(index).turnaroundTime = readyQueue.at(index).completionTime - readyQueue.at(index).arrivalTime;
+			totalTurnaround += readyQueue.at(index).turnaroundTime;
+			isCompleted[index] = 1;
+			completed++;
+			currentTime = readyQueue.at(index).completionTime;
+		}
+
+		else {
+			currentTime++;
+		}
+
+	}
+
+	float avgTurnaround;
+	avgTurnaround = (float)totalTurnaround / readyQueue.size();
+	cout << "\nPID\tArrival Time\tStart Time\tCompletion Time\tTurnaround Time\t\n";
+	for (int i = 0; i < readyQueue.size(); i++) {
+		cout << readyQueue.at(i).id << "\t" << readyQueue.at(i).arrivalTime << "\t\t" << readyQueue.at(i).startTime << "\t\t" << readyQueue.at(i).completionTime << "\t\t" << readyQueue.at(i).turnaroundTime << endl;
+	}
+
+	cout << "Average Turnaround Time = " << avgTurnaround << endl;
+
 
 }
 void RoundRobin(vector<Process> readyQueue) {
-
+	cout << "TBD" << endl;
 }
