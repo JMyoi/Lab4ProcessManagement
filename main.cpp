5db70cccf78d2304ff942939e24445cdca052485
Jay Chen <jaychenjjc@gmail.com>
2023-12-06 21:19:47 -0500

    burst time and arrival in line

--- main.cpp
+++ main.cpp
@@ -45,32 +45,14 @@ int main() {
         cout<<" "<< readyQueue.at(i).arrivalTime;
     }
 
-
-/*
-    cout << "Enter the number of processes to run: " << endl;
-    int readyQueueSize;
-    cin >> readyQueueSize;
-        vector<Process> readyQueue(readyQueueSize);
-
-    cout << "Enter the burst time, arrival time, and priority for each process: \n";
-    for (int i = 0; i < readyQueueSize; i++) {
-        cout << "Process " << i + 1 << "\n\tArrival Time: ";
-        cin >> readyQueue.at(i).arrivalTime;
-        cout << "\n\tBurst Time:";
-        cin >> readyQueue.at(i).burstTime;
-        //cout << "\n\tpriority: ";
-        //cin >> readyQueue.at(i).priority;
-        cout << endl;
-        readyQueue.at(i).id = i + 1;
-    }*/
     
    
-    cout << "****************************************Shortest Job Next************************************\n";
+    cout << "\n****************************************Shortest Job Next************************************\n";
     SJN(readyQueue);
     cout << "***********************************************************************************************\n";
     cout << "*************************************SRTF (preemptive)************************************\n";
     SRTF(readyQueue);
     cout << "***********************************************************************************************\n";
    
-    //fin.close();
+    fin.close();
 }
